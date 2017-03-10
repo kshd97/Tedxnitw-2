f18ca34964f4de2b9438cbcca6f30e7947f60f4c
priyamkhandelwal <priyamkhandelwalbtp@gmail.com>
2017-03-10 17:42:35 +0530

    small change in folder name

--- info/views.py
+++ info/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required,user_passes_test
 from django.contrib.auth.models import User
 
+from django.core.mail import send_mail
+import smtplib
+from email.mime.text import MIMEText
+
 # Create your views here.
 
 def index(request):
